4203124bf9152e4bbe04804b71bb7ec89487504d
Blatoy <loick.jeanneret@he-arc.ch>
2018-11-27 10:20:17 +0100

    Adds size to entity, closes #38

--- src/scenes/gamescene.cpp
+++ src/scenes/gamescene.cpp
@@ -1,9 +1,13 @@
 #include "gamescene.h"
 
+#include "entity.h"
+#include "rectanglecomponent.h"
+
 GameScene::GameScene(QString name, Game *game)
     : Scene(name, game)
 {
-
+    Entity *e = new Entity(this, 50, 50);
+    e->addComponent(new RectangleComponent());
 }
 
 GameScene::~GameScene()
