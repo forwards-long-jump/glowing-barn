06c47faac7e48e3006000b0a9f1ede5e524130a8
Blatoy <loick.jeanneret@he-arc.ch>
2018-11-27 10:25:35 +0100

    Removes debug

--- src/scenes/gamescene.cpp
+++ src/scenes/gamescene.cpp
@@ -6,8 +6,6 @@
 GameScene::GameScene(QString name, Game *game)
     : Scene(name, game)
 {
-    Entity *e = new Entity(this, 50, 50);
-    e->addComponent(new RectangleComponent());
 }
 
 GameScene::~GameScene()
