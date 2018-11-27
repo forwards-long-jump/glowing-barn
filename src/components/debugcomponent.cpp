e7be603e6c33381140d9b3f1b93ff53cbbe28637
Blatoy <loick.jeanneret@he-arc.ch>
2018-11-27 11:28:27 +0100

    Adds basic debug component

--- src/components/debugcomponent.cpp
+++ src/components/debugcomponent.cpp
@@ -1,16 +1,27 @@
-#include "include/rectanglecomponent.h"
+#include "include/debugcomponent.h"
 
-RectangleComponent::RectangleComponent()
+DebugComponent::DebugComponent(QString str, QColor c)
 {
-
+    debugText = str;
+    color = c;
 }
 
-void RectangleComponent::render(QPainter *painter)
+void DebugComponent::render(QPainter *painter)
 {
-    painter->fillRect(entity->boundingRect(), Qt::blue);
+    painter->fillRect(entity->boundingRect(), color);
+    int currentLineHeight = 0;
+
+    if(debugText.length() > 0) {
+        painter->drawText(entity->pos() + QPoint(0, currentLineHeight += LINE_HEIGHT), QString("%0").arg(debugText));
+    }
+
+    painter->drawText(entity->pos() + QPoint(0, currentLineHeight += LINE_HEIGHT),
+                      QString("(%0, %1) - %2x%3")
+                        .arg(entity->boundingRect().x())
+                        .arg(entity->boundingRect().y())
+                        .arg(entity->boundingRect().width())
+                        .arg(entity->boundingRect().height())
+                     );
 }
 
-void RectangleComponent::update()
-{
-    entity->setX(entity->pos().x() + 2);
-}
+void DebugComponent::update() {}
