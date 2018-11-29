7e854e845da5db7a490e1c686f7fa03559b2ecc2
Pierre Bürki <pierre.burki@he-arc.ch>
2018-11-29 11:23:47 +0100

    Fixed memory leak

--- src/scenes/gamescene.cpp
+++ src/scenes/gamescene.cpp
@@ -6,14 +6,7 @@
 GameScene::GameScene(QString name, Game *game)
     : Scene(name, game)
 {
-    Entity* test = new Entity(this);
-    test->addComponent(new RectangleComponent());
-    test->addComponent(new PlayerInputComponent());
 
-    Entity* ref = new Entity(this);
-    ref->addComponent(new RectangleComponent());
-    ref->setX(ref->pos().x() + 60);
-    ref->setY(ref->pos().y() + 60);
 }
 
 GameScene::~GameScene()
