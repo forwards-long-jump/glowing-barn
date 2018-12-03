457572ad340c4f6ba5fe3a98341fd7c24de80b33
Blatoy <loick.jeanneret@he-arc.ch>
2018-12-03 14:56:38 +0100

    Adds class attributes and methods

--- include/camera.h
+++ include/camera.h
@@ -1,11 +1,30 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include "QPointF"
+#include "entity.h"
+
 
 class Camera
 {
 public:
     Camera();
+    ~Camera();
+
+    void update();
+    void centerOn(QPointF pos, bool noSmoothing = false);
+    void attachTo(Entity *e);
+    void detachEntity();
+    void setSpeed(float speed_);
+    void setScaling(float scaling_);
+
+private:
+    QPointF targetPosition;
+    QPointF position;
+    float scaling;
+    float speed;
+
+
 };
 
 #endif // CAMERA_H
