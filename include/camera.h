27cbe2344faa7816614bc4ba81edc9df6ac5ed13
Blatoy <loick.jeanneret@he-arc.ch>
2018-12-03 22:17:25 +0100

    Adds camera main functionnalities, attachToEntity not working

--- include/camera.h
+++ include/camera.h
@@ -3,15 +3,18 @@
 
 #include "QPointF"
 #include "entity.h"
-
+#include "QGraphicsView";
 
 class Camera
 {
+    const float DEFAULT_SCALING = 1;
+    const float DEFAULT_SPEED = 1;
+
 public:
     Camera();
     ~Camera();
 
-    void update();
+    void update(QGraphicsView *v);
     void centerOn(QPointF pos, bool noSmoothing = false);
     void attachTo(Entity *e);
     void detachEntity();
@@ -19,6 +22,7 @@ public:
     void setScaling(float scaling_);
 
 private:
+    Entity *entity;
     QPointF targetPosition;
     QPointF position;
     float scaling;
--- src/camera.cpp
+++ src/camera.cpp
@@ -2,5 +2,59 @@
 
 Camera::Camera()
 {
+    position = QPointF(0,0);
+    targetPosition = QPointF(0, 0);
+    scaling = DEFAULT_SCALING;
+    speed = DEFAULT_SPEED;
+    entity = nullptr;
+}
 
+Camera::~Camera()
+{
+
+}
+
+void Camera::update(QGraphicsView *v)
+{
+    if(entity != nullptr) {
+        targetPosition.setX(entity->pos().x());
+        targetPosition.setY(entity->pos().y());
+    }
+    //position += (targetPosition - position) * speed;
+    position = targetPosition;
+   /* v->resetTransform();
+    v->scale(scaling, scaling);
+    v->setSceneRect(-position.x(), -position.y(), v->width() / scaling, v->height() / scaling);*/
+}
+
+void Camera::centerOn(QPointF pos, bool noSmoothing)
+{
+    detachEntity();
+    if(noSmoothing) {
+        targetPosition = pos;
+    }
+    else {
+        targetPosition = pos;
+        position = pos;
+    }
+}
+
+void Camera::attachTo(Entity *e)
+{
+    entity = e;
+}
+
+void Camera::detachEntity()
+{
+    entity = nullptr;
+}
+
+void Camera::setSpeed(float speed_)
+{
+    speed = speed_;
+}
+
+void Camera::setScaling(float scaling_)
+{
+    scaling = scaling_;
 }
