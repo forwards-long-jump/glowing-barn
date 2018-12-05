7ac8a746b66069c444d2e6d4dff3662e7517ddd8
Pierre Bürki <pierre.burki@he-arc.ch>
2018-12-05 14:39:30 +0100

    RE: please remove

--- src/scenes/gamescene.cpp
+++ src/scenes/gamescene.cpp
@@ -1,8 +1,4 @@
 #include "gamescene.h"
-#include "entity.h"
-#include "playerinputcomponent.h"
-#include "debugcomponent.h"
-#include "physicscomponent.h"
 
 GameScene::GameScene(QString name, Game *game)
     : Scene(name, game)
