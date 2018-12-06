626d2f78e585a27df121c0df77e0ac0adc0ce184
Blatoy <loick.jeanneret@he-arc.ch>
2018-12-06 14:25:03 +0100

    Fixes debugComponent not being rendered at the correct place

--- src/components/debugcomponent.cpp
+++ src/components/debugcomponent.cpp
@@ -8,14 +8,16 @@ DebugComponent::DebugComponent(QString str, QColor c)
 
 void DebugComponent::render(QPainter *painter)
 {
-    painter->fillRect(entity->boundingRect(), color);
+    painter->fillRect(0, 0, entity->getSize().width(), entity->getSize().height(), color);
     int currentLineHeight = 0;
 
+    // Custom text
     if(debugText.length() > 0) {
-        painter->drawText(entity->pos() + QPoint(0, currentLineHeight += LINE_HEIGHT), QString("%0").arg(debugText));
+        painter->drawText(QPoint(0, currentLineHeight += LINE_HEIGHT), QString("%0").arg(debugText));
     }
 
-    painter->drawText(entity->pos() + QPoint(0, currentLineHeight += LINE_HEIGHT),
+    // Position
+    painter->drawText(QPoint(0, currentLineHeight += LINE_HEIGHT),
                       QString("(%0, %1) - %2x%3")
                         .arg(entity->boundingRect().x())
                         .arg(entity->boundingRect().y())
