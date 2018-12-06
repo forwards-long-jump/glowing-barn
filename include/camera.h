3a731237182017952aa4475d087718aec57d843c
Blatoy <loick.jeanneret@he-arc.ch>
2018-12-06 17:48:05 +0100

    Cleanup

--- include/camera.h
+++ include/camera.h
@@ -5,20 +5,28 @@
 #include "entity.h"
 #include "QGraphicsView";
 
+// Manages what is displayed in screen per scene
 class Camera
 {
-    const float DEFAULT_SCALING = 1;
-    const float DEFAULT_SPEED = 1;
+    const float DEFAULT_SCALING = 5;
+    const float DEFAULT_SPEED = 0.2;
 
 public:
     Camera();
     ~Camera();
 
+    // Called in Game to update camera calculations
     void update(QGraphicsView *v);
+    // Center the camera on the position, use noSmoothing to teleport it
     void centerOn(QPointF pos, bool noSmoothing = false);
+    void centerOn(float x, float y, bool noSmoothing = false);
+    // Attach the camera to the entity, making it follow its center
     void attachTo(Entity *e);
+    // Detach the current entity, keep the camera in place of where it was detached
     void detachEntity();
+    // Update the speed coeff used when the camera moves
     void setSpeed(float speed_);
+    // Changes the camera scaling
     void setScaling(float scaling_);
 
 private:
--- src/camera.cpp
+++ src/camera.cpp
@@ -17,26 +17,30 @@ Camera::~Camera()
 void Camera::update(QGraphicsView *v)
 {
     if(entity != nullptr) {
-        targetPosition.setX(entity->pos().x());
-        targetPosition.setY(entity->pos().y());
+        targetPosition.setX(entity->pos().x() + entity->getSize().width() / 2);
+        targetPosition.setY(entity->pos().y() + entity->getSize().height() / 2);
     }
-    position += (targetPosition - position) * speed;
+    position += (targetPosition - position - QPointF(v->width() / scaling / 2, v->height() / scaling / 2)) * speed;
+    //position = targetPosition;
 
     v->resetTransform();
     v->scale(scaling, scaling);
-    v->setSceneRect(-position.x(), -position.y(), v->width() / scaling, v->height() / scaling);
+    //v->centerOn(targetPosition);
+    v->setSceneRect(position.x(), position.y(), v->width() / scaling, v->height() / scaling);
 }
 
 void Camera::centerOn(QPointF pos, bool noSmoothing)
 {
     detachEntity();
-    if(noSmoothing) {
-        targetPosition = pos;
-    }
-    else {
-        targetPosition = pos;
+    targetPosition = pos;
+    if(!noSmoothing) {
         position = pos;
     }
+}
+
+void Camera::centerOn(float x, float y, bool noSmoothing)
+{
+    centerOn(QPointF(x, y), noSmoothing);
 }
 
 void Camera::attachTo(Entity *e)
