f757fb91516b9357ff9a1b7cfa7a5e6c02190f2a
Blatoy <loick.jeanneret@he-arc.ch>
2018-12-14 15:12:11 +0100

    Moves debug text to DebugTextComponent

--- include/debugcomponent.h
+++ include/debugcomponent.h
@@ -2,14 +2,17 @@
 #define RECTANGLECOMPONENT_H
 
 #include "include/graphicscomponent.h"
+#include "debugtextcomponent.h"
 
 class DebugComponent : public GraphicsComponent
 {
 public:
     DebugComponent(QString name = "DebugComponent", QString debugText = "", QColor color = QColor(0, 255, 255, 127));
+    DebugComponent(QString str, QColor c);
 
     void render(QPainter* painter) override;
     void update() override;
+    void init() override;
 private:
     const int LINE_HEIGHT = 12;
     QColor color;
--- src/components/debugcomponent.cpp
+++ src/components/debugcomponent.cpp
@@ -7,28 +7,25 @@ DebugComponent::DebugComponent(QString name_, QString str, QColor c)
     color = c;
 }
 
+DebugComponent::DebugComponent(QString str, QColor c)
+    : GraphicsComponent("DebugComponent")
+{
+    debugText = str;
+    color = c;
+}
+
 void DebugComponent::render(QPainter *painter)
 {
     painter->fillRect(0, 0, entity->getSize().width(), entity->getSize().height(), color);
-    int currentLineHeight = 0;
-
-    // Custom text
-    if(debugText.length() > 0)
-    {
-        painter->drawText(QPoint(0, currentLineHeight += LINE_HEIGHT), QString("%0").arg(debugText));
-    }
-
-    // Position
-    painter->drawText(QPoint(0, currentLineHeight += LINE_HEIGHT),
-                      QString("(%0, %1) - %2x%3")
-                        .arg(entity->x())
-                        .arg(entity->y())
-                        .arg(entity->boundingRect().width())
-                        .arg(entity->boundingRect().height())
-                     );
 }
 
 void DebugComponent::update()
 {
 
+}
+
+void DebugComponent::init()
+{
+    Entity *e = new Entity(this->getEntity(), 1000, 50);
+    e->addComponent(new DebugTextComponent("DebugTextComponent", debugText));
 }
