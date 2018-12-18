62fcbbc75e7830ee5b6e06b5cfc633ba4164b1c5
Blatoy <loick.jeanneret@he-arc.ch>
2018-12-18 14:44:32 +0100

    Changes test map design

--- assets/maps/tilemap.tsx
+++ assets/maps/tilemap.tsx
@@ -1,4 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.1" name="tilemap" tilewidth="16" tileheight="16" tilecount="256" columns="16">
  <image source="tilemap.png" width="256" height="256"/>
+ <tile id="240">
+  <properties>
+   <property name="direction" value=""/>
+   <property name="h" type="int" value="0"/>
+   <property name="w" type="int" value="0"/>
+  </properties>
+ </tile>
 </tileset>
