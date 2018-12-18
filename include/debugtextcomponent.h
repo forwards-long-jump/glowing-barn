44f82df46d196eb71528fc55651cbcc84a83ee09
Blatoy <loick.jeanneret@he-arc.ch>
2018-12-18 11:13:10 +0100

    Adds the ability to change color and text

--- include/debugtextcomponent.h
+++ include/debugtextcomponent.h
@@ -6,15 +6,17 @@
 class DebugTextComponent : public GraphicsComponent
 {
 public:
-    DebugTextComponent(QString name, QString debugText);
-    DebugTextComponent(QString debugText = "");
+    DebugTextComponent(QString name, QString debugText, bool hasHitboxComponent = false);
+    DebugTextComponent(QString debugText = "", bool hasHitboxComponent = false);
 
+    void setText(const QString &text_);
     void render(QPainter* painter) override;
     void update() override;
 private:
     const int LINE_HEIGHT = 12;
     QColor color;
     QString debugText;
+    bool parentHasHitboxComponent;
 };
 
 #endif // DEBUGTEXTCOMPONENT_H
