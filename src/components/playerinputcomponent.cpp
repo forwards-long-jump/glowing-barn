e549936bc98060e5c918953f1fab6e5900d059d0
Blatoy <loick.jeanneret@he-arc.ch>
2018-12-18 11:20:46 +0100

    Adds debug changing player color depending on magnet state

--- src/components/playerinputcomponent.cpp
+++ src/components/playerinputcomponent.cpp
@@ -31,12 +31,18 @@ void PlayerInputComponent::update()
     {
         if(releasedKeyAfterTogglingMagnet)
         {
+            DebugComponent* d = dynamic_cast<DebugComponent*>(entity->getComponent("DebugComponent"));
             releasedKeyAfterTogglingMagnet = false;
             // Toggle component,
             if(!entity->disableComponent("MagneticFieldReactorComponent"))
             {
+                if(d) d->setColor(QColor(255, 0, 0, 127));
                 entity->enableComponent("MagneticFieldReactorComponent");
             }
+            else {
+                if(d) d->setColor(QColor(50, 120, 255, 127));
+            }
+
         }
     }
     else
