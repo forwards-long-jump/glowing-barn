a04ec43fa633627a0c3dac2a962a32f3e1b85c5f
Loïck Jeanneret <loick.jeanneret@he-arc.ch>
2019-01-03 21:46:53 +0100

    Makes player rotation smoother

--- src/components/playerinputcomponent.cpp
+++ src/components/playerinputcomponent.cpp
@@ -51,9 +51,10 @@ void PlayerInputComponent::update()
 
     AnimationComponent* ac = dynamic_cast<AnimationComponent*>(getEntity()->getComponent("AnimationComponent"));
     PhysicsComponent* pc = dynamic_cast<PhysicsComponent*>(getEntity()->getComponent("PhysicsComponent"));
+    int targetRotation = -qAbs(pc->getSpeed().x());
     if(ac && pc && getState() != &PlayerState::zipping)
     {
-        ac->setRotation(-qAbs(pc->getSpeed().x()));
+        ac->setRotation(ac->getRotation() + (targetRotation - ac->getRotation()) * 0.5);
     }
 
     // Handle states input
