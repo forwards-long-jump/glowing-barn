6b076ee271b8e39d00375352c0865abba9872363
Loïck Jeanneret <loick.jeanneret@he-arc.ch>
2019-01-08 00:04:41 +0100

    Adds generic magnet animation (no particles)

--- src/components/graphics/genericrendercomponent.cpp
+++ src/components/graphics/genericrendercomponent.cpp
@@ -18,3 +18,26 @@ void GenericRenderComponent::circleMagnetHitbox(QPainter* p, Entity* e, int tick
     p->setBrush(QColor(90, 105, 136, 50));
     p->drawEllipse(0, 0, diameter, diameter);
 }
+
+void GenericRenderComponent::magnetEnabledEffect(QPainter *p, Entity *e, int tick)
+{
+    QBrush redBrush();
+    QBrush blueBrush(QColor(0, 145, 228));
+
+    int entityHeight = e->getSize().height();
+    int entityWidth = e->getSize().width();
+
+    for(int i = 0; i < 3; ++i)
+    {
+        float y = entityHeight - (1 + i) * static_cast<int>((tick * 0.6)) % entityHeight;
+        float x = entityWidth * 0.5 * (1 + qSin(i + tick * 0.1));
+        p->fillRect(x, y, 2, 2, QColor(248, 0, 67, (y / entityHeight) * 255));
+    }
+
+    for(int i = 0; i < 3; ++i)
+    {
+        float y = entityHeight - (1 + i) * static_cast<int>((tick * 0.55)) % entityHeight;
+        float x = entityWidth * 0.5 * (1 + qSin(1 + i + tick * 0.1));
+        p->fillRect(x, y, 2, 2, QColor(0, 145, 228, (y / entityHeight) * 255));
+    }
+}
