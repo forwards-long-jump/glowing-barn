7e102aaa9b865df90267b6c63c26862c6f8afbfb
Pierre Bürki <pierre.burki@he-arc.ch>
2019-01-08 10:39:00 +0100

    adds required buttons to interactive components and a new map

--- src/components/hitboxes/interactivecomponent.cpp
+++ src/components/hitboxes/interactivecomponent.cpp
@@ -1,13 +1,15 @@
 #include "interactivecomponent.h"
 #include "animationcomponent.h"
+#include "gamebuttoncomponent.h"
 
-InteractiveComponent::InteractiveComponent(Input::Key key_, QString name_, QString targetHitboxName)
+InteractiveComponent::InteractiveComponent(Input::Key key_, QString name_, QString targetHitboxName, QString requiredButtons_)
     : HitboxReactorComponent(targetHitboxName, name_),
     key(key_)
 {
     readyToInteract = false;
     removePromptOnNextTick = false;
 
+    requiredButtons = GameButtonComponent::getButtonVectorFromString(requiredButtons_);
 }
 
 void InteractiveComponent::init()
@@ -39,20 +41,30 @@ void InteractiveComponent::update()
 
 void InteractiveComponent::onIntersect(HitboxComponent* hb)
 {
-    InteractiveHitboxComponent* ihb = static_cast<InteractiveHitboxComponent*> (hb);
-    showPrompt();
-    ihb->askKey(key);
-    if ((readyToInteract && ihb->isActive()) || key == Input::Key::NONE)
+    if(requiredButtons.length() == 0 || GameButtonComponent::areButtonsPressed(requiredButtons))
     {
-        this->action(hb->getParent());
-        readyToInteract = false;
-    }
-    if (!ihb->isActive())
-    {
-        readyToInteract = true;
-    }
+        InteractiveHitboxComponent* ihb = dynamic_cast<InteractiveHitboxComponent*> (hb);
+        if(ihb)
+        {
+            showPrompt();
+            ihb->askKey(key);
+            if ((readyToInteract && ihb->isActive()) || key == Input::Key::NONE)
+            {
+                this->action(hb->getParent());
+                readyToInteract = false;
+            }
+            if (!ihb->isActive())
+            {
+                readyToInteract = true;
+            }
 
-    removePromptOnNextTick = false;
+            removePromptOnNextTick = false;
+        }
+        else
+        {
+            this->action(hb->getParent());
+        }
+    }
 }
 
 void InteractiveComponent::showPrompt() const
