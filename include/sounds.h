45b13d175e705808343964cb00e61155ec54884e
Blatoy <loick.jeanneret@he-arc.ch>
2019-01-08 11:23:45 +0100

    Fixes #56

--- include/sounds.h
+++ include/sounds.h
@@ -4,6 +4,7 @@
 #include <QMediaPlayer>
 #include <QSoundEffect>
 #include <QString>
+#include <QCoreApplication>
 
 // TODO: Use a ResourceLocator if times allows it
 class Sounds
@@ -11,17 +12,21 @@ class Sounds
 public:
     static void playMusic(QString path);
     static void playSound(QString name);
-    static void init();
+
     static void fadeOut(int duration_, QString nextMusic = "");
+
+    static void setMediaPlayer(QMediaPlayer* player);
+
     static void update();
     static void loadSounds();
 private:
-    static QMediaPlayer player;
+    static QMediaPlayer* player;
+    static QMap<QString, QSoundEffect*> sounds;
+
     static int fadeOutTick;
     static int fadeoutDuration;
+
     static QString nextMusic;
-    static QMap<QString, QSoundEffect*> sounds;
-    // TODO: Preload music as well
 };
 
 #endif // SOUNDS_H
